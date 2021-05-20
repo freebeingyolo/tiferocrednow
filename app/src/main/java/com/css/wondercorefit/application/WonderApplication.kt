0f5e766deac984085de7d7b8b16ae333ca377d93
ruis <ruiszeng@yeah.net>
2021-05-20 21:04:28 +0800

    1:BUgly接入 2：dex分包接入

--- app/src/main/java/com/css/wondercorefit/application/WonderApplication.kt
+++ app/src/main/java/com/css/wondercorefit/application/WonderApplication.kt
@@ -1,14 +1,19 @@
 package com.css.wondercorefit.application
 
+import androidx.multidex.MultiDex
 import com.css.base.uibase.BaseApplication
 import com.css.wondercorefit.utils.VideoCacheHelper
+import com.tencent.bugly.Bugly
 import kotlin.properties.Delegates
 
 class WonderApplication : BaseApplication() {
     override fun onCreate() {
         super.onCreate()
+        MultiDex.install(this)
         VideoCacheHelper.init(this)
         instance = this
+        Bugly.init(getApplicationContext(), "718b817297", false);
+
     }
 
     companion object {
