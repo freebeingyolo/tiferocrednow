ee3180b411f50b357f29490bcf80719b1add939a
ruis <ruiszeng@yeah.net>
2021-07-29 18:10:48 +0800

    1：重置密码异常补充

--- lib_login/src/main/java/com/css/login/ui/ResetPasswordActivity.kt
+++ lib_login/src/main/java/com/css/login/ui/ResetPasswordActivity.kt
@@ -1,12 +1,15 @@
 package com.css.login.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.css.base.uibase.BaseActivity
+import com.css.base.utils.StringUtils
 import com.css.login.databinding.ActivityResetPasswordBinding
 import com.css.login.model.ResetPasswordViewModel
 import com.css.service.router.ARouterConst
@@ -22,6 +25,7 @@ class ResetPasswordActivity : BaseActivity<ResetPasswordViewModel, ActivityReset
         SystemBarHelper.setHeightAndPadding(this, mViewBinding.topView)
         mViewBinding.tvSubmit.setOnClickListener(this)
         mViewBinding.tvSendCode.setOnClickListener(this)
+        checkEdittext()
     }
 
     override fun registorUIChangeLiveDataCallBack() {
@@ -62,5 +66,85 @@ class ResetPasswordActivity : BaseActivity<ResetPasswordViewModel, ActivityReset
                 mViewModel.sendCode(mViewBinding.etPhone.text.toString())
             }
         }
+    }
+
+    private fun checkEdittext() {
+
+        mViewBinding.etPhone.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (mViewBinding.etPhone.text.toString().length != 11 && mViewBinding.etPhone.text.toString()
+                        .isNotEmpty()
+                ) {
+                    mViewBinding.tvPhoneTip.visibility = View.VISIBLE
+                } else if (mViewBinding.etPhone.text.toString().isEmpty()) {
+                    mViewBinding.tvPhoneTip.visibility = View.INVISIBLE
+                } else {
+                    mViewBinding.tvPhoneTip.visibility = View.INVISIBLE
+                }
+            }
+        })
+
+        mViewBinding.etSmsCode.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (mViewBinding.etSmsCode.text.toString().length != 6 && mViewBinding.etSmsCode.text.toString()
+                        .isNotEmpty()
+                ) {
+                    mViewBinding.tvCodeTip.visibility = View.VISIBLE
+                } else if (mViewBinding.etSmsCode.text.toString().isEmpty()) {
+                    mViewBinding.tvCodeTip.visibility = View.INVISIBLE
+                } else {
+                    mViewBinding.tvCodeTip.visibility = View.INVISIBLE
+                }
+            }
+        })
+        mViewBinding.etPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (mViewBinding.etPassword.text.toString().length < 6 || mViewBinding.etPassword.text.toString().length > 16) {
+                    mViewBinding.tvPasswordTip.visibility = View.VISIBLE
+                } else {
+                    mViewBinding.tvPasswordTip.visibility = View.INVISIBLE
+                }
+                if (mViewBinding.etPassword.text.toString().isEmpty()) {
+                    mViewBinding.tvPasswordTip.visibility = View.INVISIBLE
+                }
+            }
+        })
+        mViewBinding.etPasswordAgain.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (mViewBinding.etPasswordAgain.text.toString() != mViewBinding.etPassword.text.toString() && mViewBinding.etPasswordAgain.text.toString()
+                        .isNotEmpty()
+                ) {
+                    mViewBinding.tvPasswordAgainTip.visibility = View.VISIBLE
+                } else if (mViewBinding.etPasswordAgain.text.toString().isEmpty()) {
+                    mViewBinding.tvPasswordAgainTip.visibility = View.INVISIBLE
+                } else {
+                    mViewBinding.tvPasswordAgainTip.visibility = View.INVISIBLE
+                }
+            }
+        })
     }
 }
