ef216b9e01ed7d4c9e759181cbbd949224a15f41
ruis <ruiszeng@yeah.net>
2021-08-10 10:01:52 +0800

    1:登录过期登出

--- lib_base/src/main/java/com/css/base/net/interceptor/LoginInterceptor.kt
+++ lib_base/src/main/java/com/css/base/net/interceptor/LoginInterceptor.kt
@@ -1,7 +1,11 @@
 package com.css.base.net.interceptor
 
+import com.blankj.utilcode.util.ActivityUtils
 import com.blankj.utilcode.util.ToastUtils
 import com.css.base.net.HttpNetCode
+import com.css.service.router.ARouterUtil
+import com.css.service.utils.CacheKey
+import com.css.service.utils.WonderCoreCache
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -11,8 +15,15 @@ class LoginInterceptor : Interceptor {
         val response = chain.proceed(request)
         if (response.code == HttpNetCode.LOGIN_EXPIRED) {
             ToastUtils.showShort("您还未登录或登录信息已过期，请登录")
-//            logout()
+            logout()
         }
         return response
+    }
+
+    private fun logout() {
+        WonderCoreCache.removeKey(CacheKey.LOGIN_DATA)
+        WonderCoreCache.removeKey(CacheKey.USER_INFO)
+        ARouterUtil.openLogin()
+        ActivityUtils.finishAllActivitiesExceptNewest()
     }
 }
