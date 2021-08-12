2f8f5fa46b66b5a419c9c5b0e35488a7fd4c818f
zouning <zouning050@chinasoftinc.com>
2021-08-12 16:02:22 +0800

    1.优化数据统计无数据UI展示

--- app/src/main/java/com/css/wondercorefit/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/css/wondercorefit/ui/fragment/SettingFragment.kt
@@ -65,7 +65,10 @@ class SettingFragment : BaseFragment<DefaultViewModel, FragmentSettingBinding>()
                 activity?.let { MyDeviceActivity.starActivity(it) }
             }
             R.id.rl_about_us -> {
-                activity?.let {AboutUsActivity.starActivity(it)}
+                activity?.let {
+                    AboutUsActivity.starActivity(it)
+//                    DataStatisticsActivity.starActivity(requireContext(),Bundle().apply { putString("deviceType","健腹轮") })
+                }
             }
             R.id.rl_feedback -> {
                 activity?.let {
